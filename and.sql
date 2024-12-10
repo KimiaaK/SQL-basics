fbed18889b9ed0ee346a6a492dee276032be4c55
KimiaaK <keam96@ymail.com>
2024-12-09 21:16:02 -0500

    saved

---
+++ and.sql
@@ -0,0 +1,11 @@
+SELECT
+    InvoiceDate,
+    BillingAddress,
+    BillingCity,
+    total
+FROM 
+    Invoice
+WHERE
+    total>1.98 AND BillingCity LIKE 'P%' OR BillingCity LIKE 'D%'
+ORDER BY
+    InvoiceDate
